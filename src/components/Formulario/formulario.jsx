6b2635b062be40b6e3874668ed7c373212681ef1
leopuffzinho <leonardoalvesfortao@gmail.com>
2023-09-01 14:57:37 -0300

    Adicionando props

--- src/components/Formulario/formulario.jsx
+++ src/components/Formulario/formulario.jsx
@@ -15,11 +15,11 @@ function Formulario(props) {
         }}
         >
             <Typography component="h1" variant="h5">
-                Formulário de Login
+                {props.titulo}
             </Typography>
             <Box component="form" sx={{ mt: 1 }} >
-                <TextField fullWidth margin= "normal" label="Email" name="email" variant="filled" />
-                <TextField fullWidth margin="normal" label="Senha" type="password" variant="filled" />
+                <TextField fullWidth margin= "normal" label="Email" name="email" variant="filled" required/>
+                <TextField fullWidth margin="normal" label="Senha" type="password" variant="filled" required/>
                 <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Lembrar-me"
